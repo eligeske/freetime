917f5aa568d21c7da73c1f95b0123e26f01b80bb
Eli Geske <eligeske@gmail.com>
2024-12-12 22:37:51 -0500

    a

--- server.js
+++ server.js
@@ -3,12 +3,7 @@ const app = express();
 const path = require("path");
 
 // Serve static files from the chrome-app directory
-app.use(express.static("chrome-app"));
-
-// Serve the main application HTML for all routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "chrome-app/html/application.html"));
-});
+app.use(express.static(path.join(__dirname, "./")));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
